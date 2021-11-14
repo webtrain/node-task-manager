8b97682fa4925ccfed6b25874dca42a04ee1138a
laszloh <32815688+webtrain@users.noreply.github.com>
2021-11-14 20:06:54 +0100

    Set up the server & add routes & controllers

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const tasks = require('./routes/tasks')
 
+// Middlawares
+app.use(express.json())
 
+// Routes
+app.get('/', (req, res) => {
+	res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
